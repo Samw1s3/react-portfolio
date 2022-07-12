d78a8a71a90f7ee0c222640300168eb9a16e7a1a
Sam Knopper <samanthaknopper@hotmail.com>
2022-07-12 10:41:35 +0800

    contact form css

--- src/components/contact.js
+++ src/components/contact.js
@@ -48,16 +48,12 @@ function ContactForm() {
 
     return (
         <section>
-            <div style={{
-                width:'100%', display:'flex', flex:'wrap'
-            }}>
-                <form style={{display:'flex',}} id="contact-form">
-                    <div style={{
-                            width:'100%', display:'flexBlock', 
-                        }}>
-                        <label style={{width:'75px', display:'flex',}}  htmlFor="Name">Name:</label>
+            <div>
+                <form className={css.contactForm}>
+                    <div className={css.contactArea}>
+                        <label className={css.formLabel}  htmlFor="Name">Name:</label>
                         <br></br>
-                        <input style={{width:'300px', display:'flex',}}
+                        <input className={css.formInput} 
                             type="text"
                             defaultValue={name}
                             onBlur={handleBlank}
@@ -65,10 +61,10 @@ function ContactForm() {
                         />
                     </div>
                     <div>
-                        <label style={{width:'75px', display:'flex',}} htmlFor="email">Email address:</label>
+                        <label className={css.formLabel}  htmlFor="email">Email address:</label>
                         <br></br>
                         <input
-                            style={{width:'300px', display:'flex',}}
+                            className={css.formInput} 
                             type="email"
                             defaultValue={email}
                             name="email"
@@ -76,10 +72,10 @@ function ContactForm() {
                         />
                     </div>
                     <div>
-                        <label htmlFor="Message">Message:</label>
+                        <label className={css.formLabel}  htmlFor="Message">Message:</label>
                         <br></br>
                         <textarea
-                            name="Message"
+                            className={css.formInput} name="Message"
                             defaultValue={message}
                             onBlur={handleBlank}
                             rows="5"
@@ -90,7 +86,7 @@ function ContactForm() {
                             <p className="error-text">{errorMessage}</p>
                         </div>
                     )}
-                    <button style={{width:'auto', display:'block',}} type="submit">Submit</button>
+                    <button className={css.formSubmit}  type="submit">Submit</button>
                 </form>
             </div>
         </section>
