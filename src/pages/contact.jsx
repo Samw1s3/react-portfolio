b36bb7c8dbf411fd2b68c7b51f94ad2814dcfe12
Sam Knopper <samanthaknopper@hotmail.com>
2022-07-12 13:00:01 +0800

    noticed added for contact form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -7,7 +7,8 @@ export default function Contact() {
   return (
     <body className={css.body}>
       <h1>Contact</h1>
-      <p>If you would further information about me or would like to chat about something specific, please fill out the contact form below.</p>  
+      <p>If you would further information about me or would like to chat about something specific, please fill out the contact form below.</p>
+      <p className={css.alert}>Please note this contact form is currently just front-end, back-end is in development</p>  
       <ContactForm></ContactForm>
       <p>If you would like a copy of my resume please click below</p>
       <a className={css.resumeButton} href='/SamanthaKTechnicalResume.pdf' download>RESUME DOWNLOAD</a>
